4758eac8c2a8b250a553270b622e3c978e8a2079
omarluq <omaralanii@outlook.com>
2021-07-06 11:17:55 -0700

    update

---
+++ class_inheritance/manager.rb
@@ -0,0 +1,11 @@
+require_relative 'employee'
+class Manager < Employee 
+    attr_reader :employees 
+    def initialize(name, title, salary, boss, employees)
+        super(name, title, salary, boss)
+        @employees = employees
+    end
+    
+
+
+end
