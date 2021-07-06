3ec5e4784d087c6c0e725dd24c2d9dbc2b5a799a
omarluq <omaralanii@outlook.com>
2021-07-06 14:26:46 -0700

    update

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,11 +1,7 @@
 # require_relative 'board'
 class Piece
     attr_reader :color, :board, :position
-    def initialize
-        @color = color
-        @position = position
-        @board = board
-    end 
+    
     def initialize(color, position, board)
         @color = color
         @position = position
